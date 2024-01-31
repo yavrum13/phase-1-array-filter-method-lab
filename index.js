8f4f61f74b81b92800cfc9d1a33ff8eec49ae3ad
yavrum13 <yuval.avrum@gmail.com>
2024-01-31 13:47:42 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+let driversArray = [
+    { name: 'Bobby', age: 23 },
+    { name: 'Steve', age: 58},
+    { name: 'Lisa',age: 32,},
+    { name: 'Barry', age: 32,}
+]
+
+const searchLowerCase = theString.toLowerCase();
+
+findMatching();
+function findMatching(drivers, theString) {
+    const searchLowerCase = theString.toLowerCase();
+    return drivers.filter(driver => {
+      return driver.toLowerCase().indexOf(searchLowerCase) !== -1;
+    });
+}
+
+fuzzyMatch();
+function fuzzyMatch(drivers, theString){
+    const searchLowerCase = theString.toLowerCase();
+    return drivers.filter(driver => {
+        const driverLowerCase = driver.toLowerCase();
+        return driverLowerCase.toLowerCase().slice(0, 2) === searchLowerCase;
+    })
+}
+
+matchName();
+function matchName(drivers, theString) {
+    const searchLowerCase = theString.toLowerCase();
+    return drivers.filter(driver => {
+      const driverLowerCase = driver.name.toLowerCase();
+      return driverLowerCase.toLowerCase().indexOf(searchLowerCase) !== -1;
+    });
+}
